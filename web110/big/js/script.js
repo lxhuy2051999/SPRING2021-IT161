c340e6e63751ec868a1b84ff4c603d3cc4639317
lxhuy2051999 <49007183+lxhuy2051999@users.noreply.github.com>
2021-05-24 22:24:04 -0700

    final of the BIG project

---
+++ web110/big/js/script.js
@@ -0,0 +1,9 @@
+(function ($) {
+  $(document).ready(function () {
+    $("#cssmenu").menumaker({
+      title: "Menu",
+      breakpoint: 827,
+      format: "multitoggle",
+    });
+  });
+})(jQuery);
